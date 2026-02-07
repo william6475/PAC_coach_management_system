c5c36381ade48c4d232d7f3a314a7d753d435e40
William <willtrilobite@gmail.com>
2026-02-07 20:47:38 +0000

    Revert "Added webpage URLs and Views"
    This reverts commit 698705f0005e229b6e59064fc1ce77d2ebf91ba6.
    
    B

--- stupac_template/stupac/urls.py
+++ stupac_template/stupac/urls.py
@@ -3,10 +3,5 @@ from django.urls import path
 from stupac import views
 
 urlpatterns = [
-    path('', views.login, name = 'login'),
-    path('admin_home/', views.admin_home, name = 'admin_home'),
-    path('enrol_user/', views.enrol_user, name='enrol_user'),
-    path('pac_home/', views.pac_home, name='pac_home'),
-    path('student_home/', views.student_home, name='student_home'),
-    path('view_users_and_asign_pac/', views.view_users_and_asign_pac, name='view_users_and_asign_pac'),
+    path("stupac/",views.temp_here, name = "temp_here")
 ]
--- stupac_template/stupac/views.py
+++ stupac_template/stupac/views.py
@@ -4,30 +4,11 @@ from typing import Any
 from django.http import HttpResponse
 import requests
 from django.template import loader
-from django.shortcuts import render
 
 
 # Views will be created here. These will connect the database to the HTML, and is where most of the backend code will be.
 def temp_here(request):
     temp = "TemporaryVariable"
-    template = loader.get_template('Login.html')
+    template = loader.get_template('index.html')
     context = {'temp': temp}
     return HttpResponse(template.render(context, request))
-
-def login(request):
-    return render(request, "login.html")
-
-def admin_home(request):
-    return render(request, "admin_home.html")
-
-def student_home(request):
-    return render(request, "student_home.html")
-
-def pac_home(request):
-    return render(request, "pac_home.html")
-
-def enrol_user(request):
-    return render(request, "enrol_user.html")
-
-def view_users_and_asign_pac(request):
-    return render(request, "view_users_and_asign_pac.html")
