858523e905c866f79900f5229d4049548a675c93
william6475 <willtrilobite@gmail.com>
2026-02-10 14:23:31 +0000

    Update wsgi.py

--- stupac_template/PAC_coach_management_system/wsgi.py
+++ stupac_template/PAC_coach_management_system/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PAC_coach_management_system.settings')
 
 application = get_wsgi_application()
