53b5a9334591f772eb564b4c86755b982b1013a3
Ieeck <cameronwillett129@gmail.com>
2026-02-12 11:38:36 +0000

    database is here now!

--- stupac_template/stupac/models.py
+++ stupac_template/stupac/models.py
@@ -2,32 +2,42 @@ from django.db import models
 
 class Admin(models.Model):
     admin_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField()
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.CharField(max_length=64)
+
+    class Meta:
+        managed = False
+        db_table = 'admin'
 
 class Pac(models.Model):
     pac_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
     dob = models.DateField()
-    gender = models.CharField(max_length=1)
-    email = models.EmailField()
-    department = models.CharField(max_length=100)
+    gender = models.CharField(max_length=16)
+    email = models.CharField(max_length=64)
+    department = models.CharField(max_length=64)
     last_edited_when = models.DateTimeField()
     last_edited_by = models.ForeignKey(Admin, on_delete=models.CASCADE)
 
+    class Meta:
+        managed = False
+        db_table = 'pac'
+
 class Student(models.Model):
     student_id = models.AutoField(primary_key=True)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
     dob = models.DateField()
-    gender = models.CharField(max_length=1)
-    email = models.EmailField()
-    course = models.CharField(max_length=100)
+    gender = models.CharField(max_length=16)
+    email = models.CharField(max_length=64)
+    course = models.CharField(max_length=64)
     assigned_pac = models.ForeignKey(Pac, on_delete=models.CASCADE)
     last_edited_when = models.DateTimeField()
     last_edited_by = models.ForeignKey(Admin, on_delete=models.CASCADE)
 
-# ts is A-U-T-O-MATIC it's like butter
+    class Meta:
+        managed = False
+        db_table = 'student'
 
