8e713d20b58b851554ff48be668bbd3e07fbd1bb
Ieeck <cameronwillett129@gmail.com>
2026-02-12 09:51:33 +0000

    database architecture moved into models.py. sqlite file removed.

--- stupac_template/stupac/models.py
+++ stupac_template/stupac/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-#This page will be used to convert the database values into models.
+class Admin(models.Model):
+    admin_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+class Pac(models.Model):
+    pac_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    gender = models.CharField(max_length=1)
+    email = models.EmailField()
+    department = models.CharField(max_length=100)
+    last_edited_when = models.DateTimeField()
+    last_edited_by = models.ForeignKey(Admin, on_delete=models.CASCADE)
+
+class Student(models.Model):
+    student_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    dob = models.DateField()
+    gender = models.CharField(max_length=1)
+    email = models.EmailField()
+    course = models.CharField(max_length=100)
+    assigned_pac = models.ForeignKey(Pac, on_delete=models.CASCADE)
+    last_edited_when = models.DateTimeField()
+    last_edited_by = models.ForeignKey(Admin, on_delete=models.CASCADE)
+
+# ts is A-U-T-O-MATIC it's like butter
+
