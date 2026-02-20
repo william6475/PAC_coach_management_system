0df982519bddbf4737a2c91bdd1df20341be1232
William <willtrilobite@gmail.com>
2026-02-20 16:35:01 +0000

    authenticate.py now redirects users once logged in

--- authenticate.py
+++ authenticate.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
 
 from Stupac.models import Pac, Student, Admin
 
@@ -7,7 +8,17 @@ def authenticate_user(request, username, password):
     user_exists = authenticate(username=username, password=password)
     if user_exists != None:
         login(request, user_exists)
-        return("login successful")
+
+        # Redirect the logged in user to the corresponding home page
+        if user_exists.__class__.__name__ == "Student":
+            return (redirect("/student_home"))
+        elif user_exists.__class__.__name__ == "Admin":
+            return (redirect("/admin_home"))
+        elif user_exists.__class__.__name__ == "Pac":
+            return (redirect("/pac_home"))
+        else:
+            return ("User type error. An invalid user type was found with those loggin details")
+
     if user_exists == None:
 
         return("user_not_found")
